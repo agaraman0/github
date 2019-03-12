cda1f67561a7481baccc854068d17a51d98df8cc
Aman Agarwal <agaraman0@gmail.com>
2019-03-12 09:42:16 +0530

    Added Comments to webapp #7

--- Hello World/webapp.py
+++ Hello World/webapp.py
@@ -1,17 +1,17 @@
-from flask import Flask,render_template
+from flask import Flask,render_template       # import flask and flask application for web
 
 
 # Adding Comments To This File Which Needed For This Situation
 
-app = Flask(__name__)
+app = Flask(__name__)          # defining App
 
-@app.route('/')
+@app.route('/')                # giving route to the app for home page
 def home():
     return render_template('home.html')
 
-@app.route('/about/')
+@app.route('/about/')          # giving reference for about page 
 def About():
     return render_template('about.html')
 
-if __name__ == "__main__":
+if __name__ == "__main__":    # running app
     app.run(debug= True)
